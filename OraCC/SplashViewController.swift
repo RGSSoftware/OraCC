110c25d75c5691bc916dc11de80233e95a0cfed0
Randel Smith <homankids@gmail.com>
2017-02-14 15:36:26 -0600

    Add Token Invalidation logic

--- OraCC/SplashViewController.swift
+++ OraCC/SplashViewController.swift
@@ -1,4 +1,14 @@
 import UIKit
 
 class SplashViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let _ = AuthenticationToken.token {
+            performSegue(withIdentifier: "from_Splash_to_Chats", sender: nil)
+        } else {
+            performSegue(withIdentifier: "from_Splash_to_Login", sender: nil)
+        }
+    }
 }
